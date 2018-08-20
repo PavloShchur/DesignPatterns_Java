29655375ea76ec4aae63bdf65dbfd2e48bf9ca4b
Pavlo Shchur <pavloshchur@gmail.com>
2018-08-20 22:54:15 +0300

    Facade Design Pattern

--- facade/CarFacade.java
+++ facade/CarFacade.java
@@ -5,7 +5,7 @@ public class CarFacade {
     private Door door = new Door();
     private Zazhiganie zazhiganie = new Zazhiganie();
     private Wheel wheel = new Wheel();
-
+    
     public void go(){
         door.open();
         zazhiganie.fire();
