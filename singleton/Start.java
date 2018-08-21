c0046873d7a473023754d9be1e77829adb29adc1
Pavlo Shchur <pavloshchur@gmail.com>
2018-08-21 23:39:55 +0300

    Singletone Design Pattern

--- singleton/Start.java
+++ singleton/Start.java
@@ -3,10 +3,8 @@ package com.pavlo.singleton;
 public class Start {
 
     public static void main(String[] args) {
-        TestSingleton_1.getInstance().print();
-        TestSingleton_1.getInstance().print();
-        TestSingleton_1.getInstance().print();
-        
-        TestSingleton_1.getInstance().stop();
+    	Database database = Database.getInstance("Test");
+    	database.editRecord("Edited");
+    	database.editRecord("Edited 2");
     }
 }
